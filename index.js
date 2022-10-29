2252fbdad8499be8a8939d6a68e8ce31099c4024
Adedamola Adeyemo <adeyemoadedamola1@gmail.com>
2022-10-29 15:44:51 +0100

    Server created

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT;
+const port = 8080;//process.env.PORT;
 
 app.listen(
     port,
     () => console.log(`Server running on ${port}`)
 )
+const name = 'Adedamola Adeyemo';
+const backend = true;
+const age = 25;
+const bio = 'Passionate about problem solving';
 
 app.get("/", function (req, res) {
     res.json(
-        { "slackUsername": 'Adedamola Adeyemo', "backend": Yes, "age": 25, "bio": 'Passionate about problem solving' }
+        { "slackUsername": name, "backend": backend, "age": age, "bio": bio }
     )
 }
 )
